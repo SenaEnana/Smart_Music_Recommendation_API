e56fb828f0d9ec36e786b384db83283a8f472dd8
SenaEnana <senaad47@gmail.com>
2026-07-20 16:52:09 +0300

    some other route for test

--- main.py
+++ main.py
@@ -15,9 +15,26 @@ def read_search():
        "message": "Here you can search what you want!"
     }
 
+@app.get("/recommend")
+def read_recommend():
+    return {
+       "message": "Here is what I recommend based on your input!"
+    }
+
 @app.get("/all-song")
 def read_all():
     return {
-       "message": "Here you see all songs!"
+       "message": "Here you can see all songs!"
     }
 
+@app.get("/view-song")
+def read_view():
+    return {
+       "message": "Here you can view the song!"
+    }
+
+@app.get("/download")
+def read_download():
+    return {
+       "message": "Here you can download your song!"
+    }
