33629e4bbb3a87cadcbb1f3ee78f1e54bda7048b
SenaEnana <senaad47@gmail.com>
2026-07-22 15:21:05 +0300

    data adding stage started

--- database.py
+++ database.py
@@ -3,26 +3,18 @@ import sqlite3
 connection = sqlite3.connect("music-recommendation-database.db")
 cursor = connection.cursor()
 
-cursor.execute("BEGIN TRANSACTION;")
-
 cursor.execute("""
-        CREATE TABLE IF NOT EXISTS songs (
-            id INTEGER PRIMARY KEY,
-            title TEXT,
-            artist TEXT,
-            genre TEXT,
-            mood TEXT,
-            theme TEXT,
-            album TEXT,
-            language TEXT,
-            explicit INTEGER NOT NULL CHECK (explicit IN (0, 1))
-        );
-    """)
+    CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT,
+        artist TEXT,
+        genre TEXT,
+        mood TEXT,
+        theme TEXT,
+        album TEXT,
+        language TEXT,
+        explicit INTEGER NOT NULL CHECK (explicit IN (0, 1))
+    );
+""")
 
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-rows = cursor.fetchall()
-
-for row in rows:
-    print(row)
-
-connection.close()
+single_song = ("The Search", "NF", "Rap", "Calm", "Self Reflection", "The Search", "English", 0)
