039e418aa63d1c2ad6ded524f06ad70c992aa60d
SenaEnana <senaad47@gmail.com>
2026-07-23 16:06:14 +0300

    Check for duplicates (same title AND artist)

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ async def create_song(song: Song):
     try:
         connection = get_db_connection()
         cursor = connection.cursor()
+        check_query = "SELECT id FROM songs WHERE title = ? AND artist = ?"
 
         query = """
         INSERT INTO songs (title, artist, genre, mood, theme, album, language, explicit)
